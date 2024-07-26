7259cfc48de86637bf700e457fb0664f7a619e97
M Santosh <sunny962002@gmail.com>
2024-07-26 22:01:27 +0530

    Home and About Banner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 
@@ -13,6 +14,7 @@ function App() {
        <Route path="/">
          <Route index element={<Home />} />
          <Route path="contact" element={<Contact />} />
+         <Route path="about" element={<About />} />
          <Route path="*" element={<Home />} />
        </Route>
      </Routes>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import IMG1 from "../assets/HomeBanner/IMG1.png";
+import IMG2 from "../assets/HomeBanner/IMG2.jpg";
+import IMG3 from "../assets/HomeBanner/IMG3.png";
+import IMG4 from "../assets/HomeBanner/IMG4.png";
+import IMG5 from "../assets/HomeBanner/IMG5.jpg";
+import IMG6 from "../assets/HomeBanner/IMG6.jpg";
+import IMG7 from "../assets/HomeBanner/IMG7.jpg";
+import IMG8 from "../assets/HomeBanner/IMG8.jpg";
 
 function Home() {
   return (
     <div>
-      
+      <div className="homeBanner">
+        <div className="homeBannerContent">
+          <div className="heading">
+            <p>
+              Tailored Aircraft Shelters for <span>Superior Protection</span>
+            </p>
+            <sub>
+              Lorem ipsum dolor sit amet consectetur. Nam volutpat lacinia etiam
+              porta sed. At volutpat scelerisque pulvinar tincidunt id interdum
+              viverra odio
+            </sub>
+            <div className="bannerButton">
+              <Link to="/about">
+                <sup>Let’s Get Started</sup>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                >
+                  <mask
+                    id="mask0_1114_1803"
+                    style={{ maskType: "alpha" }}
+                    maskUnits="userSpaceOnUse"
+                    x="0"
+                    y="0"
+                    width="20"
+                    height="20"
+                  >
+                    <rect width="20" height="20" fill="#D9D9D9" />
+                  </mask>
+                  <g mask="url(#mask0_1114_1803)">
+                    <path
+                      d="M7.01732 16.6668L5.83398 15.4835L11.3173 10.0002L5.83398 4.51683L7.01732 3.3335L13.684 10.0002L7.01732 16.6668Z"
+                      fill="white"
+                    />
+                  </g>
+                </svg>
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="s-banner-wrapper">
+          <div className="s-banner-marquee">
+            <img src={IMG1} />
+            <img src={IMG2} />
+            <img src={IMG3} />
+            <img src={IMG4} />
+            <img src={IMG5} />
+            <img src={IMG6} />
+            <img src={IMG7} />
+            <img src={IMG8} />
+            <img src={IMG1} />
+            <img src={IMG2} />
+            <img src={IMG3} />
+            <img src={IMG4} />
+            <img src={IMG5} />
+            <img src={IMG6} />
+            <img src={IMG7} />
+            <img src={IMG8} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
